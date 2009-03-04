6328112d1a3611c4d8b9f991037c4fa6b150392e
Corey Donohoe <atmos@atmos.org>
2009-03-04 15:01:26 -0700

    small changes to this lib

--- Rakefile
+++ Rakefile
@@ -24,7 +24,7 @@ spec = Gem::Specification.new do |s|
   s.homepage = HOMEPAGE
 
   # Uncomment this to add a dependency
-  s.add_dependency "sinatra-sinatra", ">=0.9.1"
+  s.add_dependency "sinatra", ">=0.9.1"
   s.add_dependency "ruby-openid", ">=2.1.2"
 
   s.require_path = 'lib'
--- lib/sinatra/hancock/sso.rb
+++ lib/sinatra/hancock/sso.rb
@@ -1,4 +1,4 @@
-gem 'sinatra-sinatra', '~>0.9.1'
+gem 'sinatra', '~>0.9.1'
 require 'rack'
 require 'sinatra/base'
 gem 'dm-core', '~>0.9.10'
@@ -7,19 +7,31 @@ require 'dm-core'
 gem 'ruby-openid', '>=2.1.2'
 require File.dirname(__FILE__)+'/../../rack-openid'
 
+module Hancock
+  class Config
+    cattr_accessor :sso_url
+
+    def self.configure(&block)
+      yield self
+    end
+  end
+end
+
 module Sinatra
   module Hancock
     module SSO
       module Helpers
         def sso_url
-          case ENV['SINATRA_ENV']
-            when 'production'
-              'https://login.mydomain.com/sso'
-            when 'staging'
-              'https://login.staging.mydomain.com/sso'
-            else
-              'http://localhost:20000/sso'
-            end
+          ::Hancock::Config.sso_url
+        end
+        def absolute_url(suffix = nil)
+          port_part = case request.scheme
+                      when "http"
+                        request.port == 80 ? "" : ":#{request.port}"
+                      when "https"
+                        request.port == 443 ? "" : ":#{request.port}"
+                      end
+          "#{request.scheme}://#{request.host}#{port_part}#{suffix}"
         end
       end
 
