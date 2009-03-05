3822a972db89e2e0c3ca7daa8617cccb215f2878
Corey Donohoe <atmos@atmos.org>
2009-03-04 18:50:47 -0700

    fixing the specs up!

--- spec/hancock_sinatra_spec.rb
+++ spec/hancock_sinatra_spec.rb
@@ -3,11 +3,10 @@ require File.dirname(__FILE__) + '/spec_helper'
 describe Sinatra::Hancock::SSO do
   it "should protect the root url" do
     get '/'
-    @response.headers['Location'].should eql('/login')
+    @response.headers['Location'].should eql('/sso/login')
   end
   it "should greet the user when authenticated" do
     get '/', { }, :session => {:user_id => 42}
-    @response.body.should have_selector("table tbody tr td:contains('user_id')")
-    @response.body.should have_selector("table tbody tr td:contains('42')")
+    @response.body.should have_selector('p')
   end
 end
